e4a0741aa3272c087c5a93b3e25b7a66143a6057
yamheng <reasonliu012@outlook.com>
2025-10-20 15:50:02 +0800

    feat: support -s to shout

--- echo.py
+++ echo.py
@@ -1,7 +1,10 @@
 # 
+import sys  # 引入一个叫 sys 的“工具包”
+
 def echo():
+  shout = "-s" in sys.argv # 检查启动参数里有没有 "-s"
   message = input("Enter something: ")
-  print(message)
+  print(message.upper() if shout else message)
 
 if __name__ == "__main__":
   echo()
