be0c3e2aed660c14a6bf90e849f743f7735988fe
yamheng <reasonliu012@outlook.com>
2025-10-20 16:00:01 +0800

    refactor: alternative prompt on conflict branch

--- echo.py
+++ echo.py
@@ -3,7 +3,7 @@ import sys  # 引入一个叫 sys 的“工具包”
 
 def echo():
   shout = "-s" in sys.argv # 检查启动参数里有没有 "-s"
-  message = input("Enter something: ")
+  message = input("Type something: ")
   print(message.upper() if shout else message)
 
 if __name__ == "__main__":
